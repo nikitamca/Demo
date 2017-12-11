74f69f54a9993e7ef7adc6615eb3efc0fed75a13
Your Name <parmarnikita119@gmail.com>
2017-12-11 15:58:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sony1/app_login/Urls.java
@@ -0,0 +1,13 @@
+package com.example.sony1.app_login;
+
+/**
+ * Created by Sony1 on 08-12-2017.
+ */
+
+public class Urls {
+        private static final String ROOT_URL = "http://10.0.2.2/Login/v1/Api.php?apicall=";
+
+        public static final String URL_REGISTER = ROOT_URL + "signup";
+        public static final String URL_LOGIN= ROOT_URL + "login";
+}
+
---
+++ app/src/main/java/com/example/sony1/app_login/User.java
@@ -0,0 +1,59 @@
+package com.example.sony1.app_login;
+
+/**
+ * Created by Sony1 on 08-12-2017.
+ */
+
+public class User
+
+{
+    public User(int id, String username, String email, String gender) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    private String username, email, gender,password;
+    private int id;
+}
